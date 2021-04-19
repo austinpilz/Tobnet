edfa1e3795f72361f90c0665d06e739c68d177a3
BuildTools <unconfigured@null.spigotmc.org>
2021-04-18 20:18:11 -0400

    Improving ArenaChatManager javadoc

--- tobnet-game/src/main/java/com/au5tie/minecraft/tobnet/game/arena/chat/ArenaChatManager.java
+++ tobnet-game/src/main/java/com/au5tie/minecraft/tobnet/game/arena/chat/ArenaChatManager.java
@@ -6,10 +6,17 @@ import com.au5tie.minecraft.tobnet.game.arena.manager.ArenaManager;
 import com.au5tie.minecraft.tobnet.game.arena.manager.ArenaManagerType;
 import com.au5tie.minecraft.tobnet.game.arena.manager.ArenaManagerUtils;
 import com.au5tie.minecraft.tobnet.game.arena.player.ArenaPlayerManager;
-import com.au5tie.minecraft.tobnet.game.player.GamePlayer;
+import com.au5tie.minecraft.tobnet.game.exception.TobnetEngineException;
 import org.bukkit.ChatColor;
 
+/**
+ * The Arena Chat Manager controls
+ *
+ * @author au5tie
+ */
 public class ArenaChatManager extends ArenaManager {
+
+    private ArenaPlayerManager playerManager;
 
     public ArenaChatManager(TobnetArena arena) {
         super(arena);
@@ -30,20 +37,20 @@ public class ArenaChatManager extends ArenaManager {
 
     }
 
-    public void sendMessageToAllPlayers(String message) {
+    @Override
+    public void afterArenaPreparationComplete() {
+        // Link to the player manager.
+        playerManager = (ArenaPlayerManager) ArenaManagerUtils.getManagerOfType(getArena(), ArenaManagerType.PLAYER).orElseThrow(TobnetEngineException::new);
+    }
 
+
+    public void sendMessageToAllPlayers(String message) {
+        // Prepend the game's prefix to the message we'll send.
         String newMessage = ChatColor.RED + TobnetGamePlugin.chatPrefix + ChatColor.RESET + message;
 
-        ArenaPlayerManager playerManager = ArenaManagerUtils.getPlayerManager(getArena()).get();
-
-        playerManager.getPlayers()
+        // Send the message to all current players.
+        playerManager.getPlayers().stream()
+                .filter(player -> player.getPlayer().isOnline()) // Ensure we don't try to send messages to offline players.
                 .forEach(player -> player.getPlayer().sendMessage(newMessage));
-
-
-
-        for (GamePlayer player : playerManager.getPlayers()) {
-            //player.getPlayer().spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(message));
-        }
-
     }
 }
